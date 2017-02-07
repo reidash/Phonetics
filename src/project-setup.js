a91fdbdc8a4e45f5b2644f0e2c0adb3259549c09
Cindy Xie <x22xie@uwaterloo.ca>
2017-02-07 18:14:28 -0500

    Cleaning up after renaming folder

--- src/project-setup.js
+++
@@ -1,7 +0,0 @@
-var util = require('./tasks/util');
-var exec = require('child_process').exec;
-var platforms = require('./tasks/platforms');
-var plugins = require('./tasks/plugins');
-
-exec(platforms);
-exec(plugins);
