b0c1330302f549f7e29f46238a8e47c97d96e7de
Cindy Xie <x22xie@uwaterloo.ca>
2017-02-20 11:39:10 -0500

    Randomizer code to util class

--- Project/src/pages/ListeningMode/ListeningMode.ts
+++ Project/src/pages/ListeningMode/ListeningMode.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import Util from '../../util';
 
 @Component({
-  selector: 'page-ListeningMode',
-  templateUrl: 'ListeningMode.html'
+    selector: 'page-ListeningMode',
+    templateUrl: 'ListeningMode.html'
 })
 export class ListeningMode {
-  title: string = 'Listening Mode'; //this should be the title of the phoneme list
+    title: string = 'Listening Mode'; //this should be the title of the phoneme list
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    //constructor code here
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        //constructor code here
+        let test = ["Saab", "Volvo", "BMW", "nissan", "lala"];
+        test = Util.shuffle(test);
+
+        test.forEach(function (val, ind) {
+            document.writeln(val);
+        });
+    }
 }
 
